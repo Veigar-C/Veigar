468fd329c4ce194e605a7a436c972152b6bf2813
wscwj8 <1164358872@qq.com>
2017-12-29 17:00:58 +0800

    IDEA_GIT_2

--- src/main/java/com/veigar/service/IUserService.java
+++ src/main/java/com/veigar/service/IUserService.java
@@ -4,12 +4,13 @@ package com.veigar.service;
  * Created by Administrator on 2017/10/9.
  */
 import com.veigar.model.DrivingLicense;
-import com.veigar.model.User;
 import com.veigar.model.ViolationRecord;
+
+import java.util.List;
 
 public interface IUserService {
 
-     User selectUser(long userId);
      DrivingLicense selectDriver(String carNum);
-     ViolationRecord selectRecord(String carNum);
+     List<ViolationRecord> selectRecord(String carNum);
+     DrivingLicense dataFilter(DrivingLicense drivingLicense);
 }
